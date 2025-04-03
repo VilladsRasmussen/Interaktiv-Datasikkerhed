32753c3a923e2dbe55439a1b62f1048a7fcca214
FearTheV <105357507+VilladsRasmussen@users.noreply.github.com>
2025-04-03 21:22:51 +0200

    Initialize observerende, sikkerhed, and penge variables; add conditional logging for observation and security status

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,29 @@
-let observerende
-let sikkerhed
-let penge
+let observerende = 0;
+let sikkerhed = 0;
+let penge = 5000;
+
+if (observerende >= 0) {
+    console.log("Du er observerende!")
+  } else if (observerende >= 20) {
+    console.log("Du er meget lidt observerende!")
+  } else if (observerende >= 40) {
+    console.log("Du er en smule observerende!")
+  } else if (observerende >= 60) {
+    console.log("Du er godt observerende!")
+  } else if (observerende >= 80) {
+    console.log("Du er fremragende til at observere!")
+  } else if (observerende < 0) {
+    console.log("Du er ikke observerende nok!");
+}
+
+if (sikkerhed >= 0) {
+    console.log("Du er meget sikker!");
+}
+
+if (penge = 5000) {
+    console.log("Ingen kan stjæle dine penge!");
+  } else if (penge < 5000) {
+    console.log("Nogle har været succesfuld i at stjæle dine penge, men heldigvis tog du handling og fik reddet de restende penge!");
+  } else if (observerende = 0) {
+    console.log("Glad for du ikke er min bankrådgiver!");
+}
