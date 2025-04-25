b6eddce7c73a71960b41da683644d497ec83df19
FearTheV <villadsrasmussen1@gmail.com>
2025-04-25 10:31:00 +0200

    Refactor index.html structure; update button functionality and feedback display in script.js; enhance SCSS styles and variables for improved design consistency.

--- js/script.js
+++ js/script.js
@@ -1,65 +1,26 @@
 'use strict';
 
+// Start
 
-// ADGANGSKODE TJEKNING:
-// ------------------------------------------------------
+// NODELIST / ARRAY
+const btns = document.querySelectorAll('.btn-group button');
+const boxes = document.querySelectorAll('.feedback');
+const choices = []
 
-function vurderAdgangskode(strofe) {
-    let styrke = 0;
-
-    // Længde
-    if (strofe.length >= 8) styrke += 1; // Min 8 tegn
-
-    // Indeholder både store og små bogstaver
-    if (/[a-z]/.test(strofe) && /[A-Z]/.test(strofe)) styrke += 1;
-
-    // Indeholder tal
-    if (/\d/.test(strofe)) styrke += 1;
-
-    // Indeholder specialtegn
-    if (/[!@#$%^&*(),.?":{}|<>]/.test(strofe)) styrke += 1;
-
-    // Tjek om adgangskoden ikke indeholder ord eller talrækkefølger
-    if (!/1234|password|qwerty|abc/.test(strofe.toLowerCase())) styrke += 1;
-
-    // Vurder adgangskodens styrke
-    if (styrke <= 1) {
-        return 'Dårlig';
-    } else if (styrke === 2) {
-        return 'Middel';
-    } else {
-        return 'God';
+// FUNCTION
+const checkAnswer = (e) => { // Funktion med en parameter (e)
+    boxes.forEach(box => {
+        box.style.display = 'none';
+    })
+    choices.push(e.target.id); // Tilføjer id'et fra det klikkede element til choices arrayet
+    console.log(choices); // Logger choices arrayet i konsollen
+    switch (e.target.id) { // Forholder sig til det klikkede elements id
+        case 'c1': document.querySelector('#c1-f').style.display = 'block'; // Viser elementet med id="c1-f"
+        break;
+        default: console.log("Error"); // Viser error, hvis id ikke findes
     }
 }
 
-// button example
-
-const c1 = document.querySelector('#c1');
-const c2 = document.querySelector('#c2');
-const c3 = document.querySelector('#c3');
-
-const checkAnswer = (e) => { // e 
-    console.log(e.target);
-    
-    if (e.target.id === 'c1') {
-        console.log('That is correct!');
-    } else {
-        console.log('That is wrong!')
-    }
-}
-
-/* const checkAnswer = (e) => { // e 
-    console.log(e.target);
-    
-    if (e.target.id !== 'c1') {
-        console.log('That is correct!');
-    } else {
-        console.log('That is wrong!')
-    }
-}
-
-*/
-
-c1.addEventListener('click', checkAnswer);
-c2.addEventListener('click', checkAnswer);
-c3.addEventListener('click', checkAnswer);
+btns.forEach(btn => {
+    btn.addEventListener('click', checkAnswer);
+})
