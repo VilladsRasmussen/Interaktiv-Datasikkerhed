e727a83caf9280120887a8f444720b49d94f4192
FearTheV <villadsrasmussen1@gmail.com>
2025-05-02 10:34:39 +0200

    Refactor JavaScript and SCSS files; update display styles to flex, remove unused files, and enhance styling structure with variables and mixins.

--- js/script.js
+++ js/script.js
@@ -20,19 +20,17 @@ const checkAnswer = (e) => {
     switch (e.target.id) {
     // START
         case 'start': 
-            document.querySelector('#trin1').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#trin1').style.display = 'flex';
         break;
 
     // TRIN 1 - Lav en brugernavn
         case 'godkend1': 
             const inputValue = document.getElementById('kaldenavn').value;
             choices.push(inputValue);
-            document.querySelector('#trin2').style.display = 'block';
+            document.querySelector('#trin2').style.display = 'flex';
             const displayElement = document.querySelector('#visKaldenavn');
             if (displayElement) {
                 displayElement.textContent = inputValue;
-                console.log('Choices array:', choices);
             }
         break;
 
@@ -40,35 +38,29 @@ const checkAnswer = (e) => {
 
     // VALG 1 - Indtastede du rigtige eller falsk navn?
         case 'rigtige': 
-            document.querySelector('#svar1a').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#svar1a').style.display = 'flex';
         break;
         case 'falske': 
-            document.querySelector('#svar1b').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#svar1b').style.display = 'flex';
         break;
 
 
 
     // SVAR 1.1
         case 'forsæt1a': // Rettet fra 'fortsæt1a' til 'forsæt1a'
-            document.querySelector('#trin2a').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#trin2a').style.display = 'flex';
         break;
         case 'forsæt1b': 
-            document.querySelector('#trin2b').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#trin2b').style.display = 'flex';
         break;
 
 
     // Feedback 1a og 1b
         case 'forsæt1aa': // Rettet fra 'fortsæt1a' til 'forsæt1a'
-            document.querySelector('#retning1').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#retning1').style.display = 'flex';
         break;
         case 'forsæt1bb': 
-            document.querySelector('#retning2').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#retning2').style.display = 'flex';
         break;
 
 
@@ -78,93 +70,75 @@ const checkAnswer = (e) => {
 
     // Retning 1 - Will Smith
         case 'bloker': 
-            document.querySelector('#svar2a').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#svar2a').style.display = 'flex';
         break;
         case 'klik': 
-            document.querySelector('#svar2b').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#svar2b').style.display = 'flex';
         break;
 
 
     // SVAR 2.1
         case 'forsæt2a': // Rettet fra 'fortsæt2a' til 'forsæt2a'
-            document.querySelector('#trin3a').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#trin3a').style.display = 'flex';
         break;
         case 'forsæt2b': 
-            document.querySelector('#trin3b').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#trin3b').style.display = 'flex';
         break;
 
     // Feedback 2a og 2b
         case 'slutning1': // Rettet fra 'fortsæt2a' til 'forsæt2a'
-            document.querySelector('#konklusion1').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#konklusion1').style.display = 'flex';
         break;
         case 'slutning2': 
-            document.querySelector('#konklusion2').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#konklusion2').style.display = 'flex';
         break;
 
     // SLUTNING 1 - Will Smith snød dig ikke
         case 'afrunding1': 
-            document.querySelector('#endelig-feedback1').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#endelig-feedback1').style.display = 'flex';
         break;
         case 'afrunding2': 
-            document.querySelector('#endelig-feedback2').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#endelig-feedback2').style.display = 'flex';
         break;
-
 
 
 
 
     // Retning 2 - Anti Virus
         case 'klik-knap': 
-            document.querySelector('#svar3a').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#svar3a').style.display = 'flex';
         break;
         case 'ignorer': 
-            document.querySelector('#svar3b').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#svar3b').style.display = 'flex';
         break;
 
     // SVAR 3.1
         case 'forsæt3a': // Rettet fra 'fortsæt2a' til 'forsæt2a'
-            document.querySelector('#feedback4a').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#feedback4a').style.display = 'flex';
         break;
         case 'forsæt3b': 
-            document.querySelector('#feedback4b').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#feedback4b').style.display = 'flex';
         break;
 
     // Konsekvens
         case 'konsekvens': // Rettet fra 'fortsæt2a' til 'forsæt2a'
-            document.querySelector('#du-er-hacket').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#du-er-hacket').style.display = 'flex';
         break;
         case 'slutning3': // Rettet fra 'fortsæt2a' til 'forsæt2a'
-            document.querySelector('#konklusion3').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#konklusion3').style.display = 'flex';
         break;
 
     // Feedback 3b
         case 'slutning4': 
-            document.querySelector('#konklusion4').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#konklusion4').style.display = 'flex';
         break;
 
     // SLUTNING 3 & 4
         case 'afrunding3': 
-            document.querySelector('#endelig-feedback3').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#endelig-feedback3').style.display = 'flex';
         break;
         case 'afrunding4': 
-            document.querySelector('#endelig-feedback4').style.display = 'block';
-            console.log('Choices array:', choices);
+            document.querySelector('#endelig-feedback4').style.display = 'flex';
         break;
 
 
